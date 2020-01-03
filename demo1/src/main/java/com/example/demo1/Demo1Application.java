3befd86828cdc984d204db42eeba0a5c19d04b61
wusujie <191621738@qq.com>
2020-01-03 11:43:23 +0800

    添加了SpringBoot的注解的注释

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//这个是springboot的入口，主函数
 @SpringBootApplication
 //SpringBoot不能自动扫描mapper
 @MapperScan("com.example.demo1.mapper")
